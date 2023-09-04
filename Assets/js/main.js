7efa67c4206a42891c96e56ad993dd9d8dcc577b
rsiduran <120078285+rsiduran@users.noreply.github.com>
2023-09-04 16:36:10 +0800

    Update

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,1 +1,14 @@
-import 'boxicons';
+const hamburger = document.querySelector(".hamburger");
+const navSections = document.querySelector(".nav-sections");
+const navBar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navSections.classList.toggle("active");
+    navBar.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+hamburger.classList.remove("active");
+navSections.classList.remove("active");
+}))
